f31a718b6740f067d57ebadc9ca2a0387613d681
Akash Mulye <akashmulye97@gmail.com>
2024-01-16 15:41:52 -0500

    initial landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Landing } from "./Components/Landing"
+
 export const App = () => {
     return (
         <>
-        
+            <main className='page'>
+                <Landing />
+            </main>
         </>
     )
 }
